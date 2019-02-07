ef42c4c1e97f70761f70b324a63199d0ea15941a
Daniel Salas <danielsalas074@gmail.com>
2019-02-06 20:45:17 -0700

    added Job role field switch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 //this sets a focus on the first text field
 $(document).ready(function() {
     $('form:first *:input[type!=hidden]:first').focus();
+    $('#other-title').hide();//job role field is not shown by default
+
+  //shows the job role field when other is selected
+  $("#show").click(function()
+  {
+    $('#other-title').show();
+  });
+  //hides job role field when other is deselected
+  $(".hid").click(function()
+  {
+    $('#other-title').hide();
+  });
+
+
 });
